e763b38f38526523671065880a2c1d8e61de1aa4
Fabien Anthonioz <fabien.anthonioz@neteden.fr>
2017-07-24 16:11:30 +0200

    Adjusts the config specs

--- spec/lib/syspay-sdk/config_spec.rb
+++ spec/lib/syspay-sdk/config_spec.rb
@@ -1,120 +1,124 @@
 require 'spec_helper'
 
 describe SyspaySDK::Config do
-  Config = SyspaySDK::Config
-
   it "loads configuration file and default environment" do
-    lambda do
-      Config.load("spec/config/syspay.yml", "test")
-      Config.default_environment.should eq("test")
-    end.should_not raise_error
+    expect {
+      described_class.load("spec/config/syspay.yml", "test")
+      expect(described_class.default_environment).to eq("test")
+    }.not_to raise_error
+  end
+
+  before :each do
+    @backup_default_environment = described_class.default_environment
+    @backup_configurations = described_class.configurations
+  end
+
+  after :each do
+    described_class.default_environment = @backup_default_environment
+    described_class.configurations = @backup_configurations
   end
 
   it "sets default environment" do
-    begin
-      backup_default_environment = Config.default_environment
-      Config.default_environment = "new_env"
-      Config.default_environment.should eq("new_env")
-    ensure
-      Config.default_environment = backup_default_environment
-    end
+    described_class.default_environment = "new_env"
+
+    expect(described_class.default_environment).to eq("new_env")
   end
 
   it "sets configuration values" do
-    begin
-      backup_configurations = Config.configurations
-      Config.configurations = { Config.default_environment => { :syspay_id => "direct", :syspay_passphrase => "direct" } }
-      Config.config.syspay_id.should eq("direct")
-      Config.config.syspay_passphrase.should eq("direct")
-    ensure
-      Config.configurations = backup_configurations
-    end
+    described_class.configurations = { described_class.default_environment => { :syspay_id => "direct", :syspay_passphrase => "direct" } }
+
+    expect(described_class.config.syspay_id).to eq("direct")
+    expect(described_class.config.syspay_passphrase).to eq("direct")
   end
 
   it "configures using parameters" do
-    begin
-      backup_configurations = Config.configurations
-      Config.configurations = nil
-      Config.configure( :syspay_id => "Testing" )
-      Config.config.syspay_id.should eq("Testing")
-    ensure
-      Config.configurations = backup_configurations
-    end
+    described_class.configurations = nil
+    described_class.configure( :syspay_id => "Testing" )
+
+    expect(described_class.config.syspay_id).to eq("Testing")
   end
 
   it "configures with block" do
-    begin
-      backup_configurations = Config.configurations
-      Config.configurations = nil
-      Config.configure do |config|
-        config.syspay_id = "Testing"
-      end
-      Config.config.syspay_id.should eq("Testing")
-    ensure
-      Config.configurations = backup_configurations
+    described_class.configurations = nil
+    described_class.configure do |config|
+      config.syspay_id = "Testing"
     end
+
+    expect(described_class.config.syspay_id).to eq("Testing")
   end
 
   it "configure with default values" do
-    begin
-      backup_configurations = Config.configurations
-      default_config = Config.config
-      Config.configure do |config|
-        config.syspay_id = "Testing"
-      end
-      Config.config.syspay_id.should eq("Testing")
-      Config.config.syspay_mode.should_not be_nil
-      Config.config.syspay_mode.should eq(default_config.syspay_mode)
-    ensure
-      Config.configurations = backup_configurations
+    default_config = described_class.config
+
+    described_class.configure do |config|
+      config.syspay_id = "Testing"
     end
+
+    expect(described_class.config.syspay_id).to eq("Testing")
+    expect(described_class.config.syspay_mode).to_not be_nil
+    expect(described_class.config.syspay_mode).to eq(default_config.syspay_mode)
   end
 
   it "validates configuration" do
-    config = Config.new( :syspay_id => "XYZ")
-    lambda { config.required!(:syspay_id) }.should_not raise_error
-    lambda { config.required!(:syspay_passphrase) }.should raise_error "Required configuration(syspay_passphrase)"
-    lambda { config.required!(:syspay_id, :syspay_passphrase) }.should raise_error "Required configuration(syspay_passphrase)"
-    lambda { config.required!(:syspay_passphrase, :syspay_mode) }.should raise_error "Required configuration(syspay_passphrase, syspay_mode)"
+    config = described_class.new( :syspay_id => "XYZ")
+
+    expect {
+      config.required!(:syspay_id)
+    }.to_not raise_error
+
+    expect {
+      config.required!(:syspay_passphrase)
+    }.to raise_error "Required configuration(syspay_passphrase)"
+
+    expect {
+      config.required!(:syspay_id, :syspay_passphrase)
+    }.to raise_error "Required configuration(syspay_passphrase)"
+
+    expect {
+      config.required!(:syspay_passphrase, :syspay_mode)
+    }.to raise_error "Required configuration(syspay_passphrase, syspay_mode)"
   end
 
   it "returns default environment configuration" do
-    Config.config.should be_a(Config)
+    expect(described_class.config).to be_a(described_class)
   end
 
   it "returns configuration based on environment" do
-    Config.config(:development).should be_a(Config)
+    expect(described_class.config(:development)).to be_a(described_class)
   end
 
   it "overrides default configuration" do
     override_configuration = { :syspay_id => "test.example.com", :syspay_mode => "test"}
-    config = Config.config(override_configuration)
+    config = described_class.config(override_configuration)
 
-    config.syspay_id.should eq(override_configuration[:syspay_id])
-    config.syspay_mode.should eq(override_configuration[:syspay_mode])
+    expect(config.syspay_id).to eq(override_configuration[:syspay_id])
+    expect(config.syspay_mode).to eq(override_configuration[:syspay_mode])
   end
 
   it "gets cached config" do
-    Config.config(:test).should eq(Config.config(:test))
-    Config.config(:test).should_not eq(Config.config(:development))
+    expect(described_class.config(:test)).to eq(described_class.config(:test))
+    expect(described_class.config(:test)).to_not eq(described_class.config(:development))
   end
 
   it "raises an error on invalid environment" do
-    lambda { Config.config(:invalid_env) }.should raise_error "Configuration[invalid_env] NotFound"
+    expect { described_class.config(:invalid_env) }.to raise_error "Configuration[invalid_env] NotFound"
   end
 
   it "sets logger" do
     require 'logger'
     my_logger = Logger.new(STDERR)
-    Config.logger = my_logger
-    Config.logger.should eq(my_logger)
+
+    described_class.logger = my_logger
+    expect(described_class.logger).to eq(my_logger)
   end
 
   it "has access to SyspaySDK methods" do
-    SyspaySDK.configure.should eq(SyspaySDK::Config.config)
-    SyspaySDK.logger.should eq(SyspaySDK::Config.logger)
+    expect(SyspaySDK.configure).to eq(SyspaySDK::Config.config)
+    expect(SyspaySDK.logger).to eq(SyspaySDK::Config.logger)
+
     SyspaySDK.logger = SyspaySDK.logger
-    SyspaySDK.logger.should eq(SyspaySDK::Config.logger)
+
+    expect(SyspaySDK.logger).to eq(SyspaySDK::Config.logger)
   end
 
   describe "::Configuration" do
@@ -122,52 +126,54 @@ describe SyspaySDK::Config do
       include SyspaySDK::Configuration
     end
 
+    subject { TestConfig.new }
+
     it "gets default configuration" do
-      test_object = TestConfig.new
-      test_object.config.should be_a(Config)
+      expect(subject.config).to be_a(described_class)
     end
 
     it "changes environment" do
-      test_object = TestConfig.new
-      test_object.set_config("test")
-      test_object.config.should eq(Config.config("test"))
-      test_object.config.should_not eq(Config.config("development"))
+      subject.set_config("test")
+
+      expect(subject.config).to eq(described_class.config("test"))
     end
 
     it "overrides environment configuration" do
-      test_object = TestConfig.new
-      test_object.set_config("test", :syspay_id => "test")
-      test_object.config.should_not eq(Config.config("test"))
+      subject.set_config("test", :syspay_id => "test")
+
+      expect(subject.config).to_not eq(described_class.config("test"))
     end
 
     it "overrides default/current configuration" do
-      test_object = TestConfig.new
-      test_object.set_config( :syspay_id => "test")
-      test_object.set_config( :syspay_passphrase => "test")
-      test_object.config.syspay_passphrase.should eq("test")
-      test_object.config.syspay_id.should eq("test")
+      subject.set_config( :syspay_id => "test")
+      subject.set_config( :syspay_passphrase => "test")
+
+      expect(subject.config.syspay_passphrase).to eq("test")
+
+      expect(subject.config.syspay_id).to eq("test")
     end
 
     it "appends ssl_options" do
-      test_object = TestConfig.new
-      test_object.set_config( :ssl_options => { :ca_file => "test_path" } )
-      test_object.config.ssl_options[:ca_file].should eql "test_path"
-      test_object.set_config( :ssl_options => { :verify_mode => 1 } )
-      test_object.config.ssl_options[:verify_mode].should eql 1
-      test_object.config.ssl_options[:ca_file].should eql "test_path"
+      subject.set_config( :ssl_options => { :ca_file => "test_path" } )
+
+      expect(subject.config.ssl_options[:ca_file]).to eql "test_path"
+      subject.set_config( :ssl_options => { :verify_mode => 1 } )
+
+      expect(subject.config.ssl_options[:verify_mode]).to eql 1
+
+      expect(subject.config.ssl_options[:ca_file]).to eql "test_path"
     end
 
     it "sets configuration without loading configuration File" do
-      backup_configurations = Config.configurations
-      begin
-        Config.configurations = nil
-        test_object = TestConfig.new
-        lambda { test_object.config }.should raise_error
-        test_object.set_config( :syspay_id => "test" )
-        test_object.config.should be_a(Config)
-      ensure
-        Config.configurations = backup_configurations
-      end
+      described_class.configurations = nil
+
+      expect {
+        subject.config
+      }.to raise_error Errno::ENOENT
+
+      subject.set_config( :syspay_id => "test" )
+
+      expect(subject.config).to be_a(described_class)
     end
   end
 end
