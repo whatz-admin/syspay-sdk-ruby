2083fef351f7e9e47ffa770ff303089ceb7bcf43
cle-captive <clement@captive.fr>
2020-02-17 11:30:44 +0100

    Can add an amount for the refund

--- lib/syspay-sdk/requests/refund.rb
+++ lib/syspay-sdk/requests/refund.rb
@@ -4,7 +4,7 @@ module SyspaySDK
       METHOD = 'POST'.freeze
       PATH = '/api/v1/merchant/refund'.freeze
 
-      attr_accessor :payment_id, :ems_url, :refund
+      attr_accessor :payment_id, :ems_url, :refund, :amount, :currency, :description
 
       def build_response(response)
         raise SyspaySDK::Exceptions::BadArgumentTypeError, 'response must be a Hash' unless response.is_a?(Hash)
@@ -16,7 +16,9 @@ module SyspaySDK
       def data
         hash = {}
         hash = refund.to_hash unless refund.nil?
-        hash[:payment_id] = payment_id
+        %i[payment_id amount currency description].each do |attribute|
+          hash[attribute] = send(attribute)
+        end
         hash[:ems_url] = ems_url unless ems_url.nil?
         hash
       end
