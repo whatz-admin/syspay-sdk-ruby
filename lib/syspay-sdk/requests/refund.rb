7f502447b8d527fcfa1cf0b48e51bb3d88b20109
Clément Prod'homme <prodhomme.clement@hotmail.fr>
2020-04-01 11:09:21 +0200

    add `reference` attribute for SyspaySDK::Requests::Refund

--- lib/syspay-sdk/requests/refund.rb
+++ lib/syspay-sdk/requests/refund.rb
@@ -4,7 +4,7 @@ module SyspaySDK
       METHOD = 'POST'.freeze
       PATH = '/api/v1/merchant/refund'.freeze
 
-      attr_accessor :payment_id, :ems_url, :refund, :amount, :currency, :description
+      attr_accessor :payment_id, :reference, :ems_url, :refund, :amount, :currency, :description
 
       def build_response(response)
         raise SyspaySDK::Exceptions::BadArgumentTypeError, 'response must be a Hash' unless response.is_a?(Hash)
@@ -16,7 +16,7 @@ module SyspaySDK
       def data
         hash = {}
         hash = refund.to_hash unless refund.nil?
-        %i[payment_id amount currency description].each do |attribute|
+        %i[payment_id reference amount currency description].each do |attribute|
           hash[attribute] = send(attribute)
         end
         hash[:ems_url] = ems_url unless ems_url.nil?
